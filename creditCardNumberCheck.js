99ae41f531976fc67c090ab56e6b664d6fe00599
Ákos Nikházy <nikhazy.akos@gmail.com>
2013-07-25 12:08:22 +0200

    Update creditCardNumberCheck.js

--- creditCardNumberCheck.js
+++ creditCardNumberCheck.js
@@ -1,11 +1,14 @@
 (function ( $ ) {
-  var numToTen=0,subTotal = 0,evenCheck,evenSplit,lastNum;
+  var numToTen,subTotal,evenCheck,evenSplit,lastNum;
   
   $.fn.isCreditCard = function() {
     var n = this.val();
     
     //Can't be card number if not 16 char long or not a number. return false
     if(n.length != 16 || isNaN(n)){return false;}
+    
+    //Set everything
+    numToTen=subTotal=evenCheck=evenSplit=lastNum=0;
     
     //From here it use the Hans Peter Luhn algorithm to check the card number
     var nums = n.split('');
