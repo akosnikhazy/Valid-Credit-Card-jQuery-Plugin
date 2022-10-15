fcafcabd7bc1d14cbd3d91ede4c9afb61dfecf7d
Ákos Nikházy <nikhazyakos@tutanota.com>
2022-10-15 13:02:44 +0200

    Update creditCardNumberCheck.js

--- creditCardNumberCheck.js
+++ creditCardNumberCheck.js
@@ -7,15 +7,12 @@
     // we just stop here.
     if(this.val().length != 16 || isNaN(this.val())) {return false;}
     
-    // Set everything
-    var digits = this.val().split('').map(Number);
-	
     // From here it use the Hans Peter Luhn algorithm to check the card number
     // How it works is you double the value of every second digit, if the doubled
     // value is bugger than 9 you add up the digits of that number and at the end
     // you add up all the digits you made this way. If the result is dividable with
     // ten it is a valid credit card number.
-    for(var i = 14; i >= 0; i -= 2)
+    for(var digits = this.val().split('').map(Number), i = 14; i >= 0; i -= 2)
     {
 	if(digits[i]*2 > 9)
 	{
@@ -28,7 +25,6 @@
     }
 	
     // if the total is not dividable by ten it is not valid
-    if(digits.reduce((a, b) => a + b, 0)%10 !== 0) return false;
+    return digits.reduce((a, b) => a + b, 0)%10 == 0;
 	  
-    return true;
 };}( jQuery ));
